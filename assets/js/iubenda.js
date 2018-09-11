1193b8f8d45add66c5d1ebeac606ccd83f7df044
Michael Bryant <shadow53@shadow53.com>
2018-09-11 11:38:18 -0700

    Improve Iubenda loading
    Replace dynamic insertion of a script tag with the script tag itself (at
    the end of the body) and replace the cookie banner JS file with the
    versioned and cacheable one that users will get redirected to. The only
    downside of this is having to manually update that URL when new versions
    are released.

--- assets/js/iubenda.js
+++ assets/js/iubenda.js
@@ -1,20 +1,3 @@
-{{ if and $.Site.Params.iubenda.id (or (not $.Site.Params.iubenda.pro.enable_pro) (not $.Site.Params.iubenda.pro.disable_footer_link)) }}
-(function (w,d) {
-    var loader = function () {
-        var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0];
-        s.src = "//cdn.iubenda.com/iubenda.js";
-        tag.parentNode.insertBefore(s,tag);
-    };
-    if (w.addEventListener) {
-        w.addEventListener("load", loader, false);
-    } else if (w.attachEvent) {
-        w.attachEvent("onload", loader);
-    } else {
-        w.onload = loader;
-    }
-})(window, document);
-{{ end }}
-
 {{- if and (not $.Site.IsServer) (and $.Site.Params.iubenda.pro.enable_cookie_policy (and $.Site.Params.iubenda.id $.Site.Params.iubenda.pro.site_id)) -}}
 var _iub = _iub || [];
 _iub.csConfiguration = {
